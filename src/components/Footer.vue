934f4c1dc8fe383ad37a99fe681991bc77dfa01d
Diego <diegocmd20@gmail.com>
2024-10-25 20:12:19 -0300

    First merge of feature header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container-fluid bg-info-subtitle position-absolute bottom-0 end-0">
+        <div class="d-flex justify-content-center align-items-center">
+            <h6 class="display-6 fw-semibold">CV-Vue. Todos los derechos reservados.</h6>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="display-3">Home</h1>
 </template>
